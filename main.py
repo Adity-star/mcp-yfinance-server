25bb7f4c95512c7b440b94390b560678daffb97d
Hafif Ashiq <hafifashiq07@gmail.com>
2025-08-11 16:23:19 +0500

    Changed streamable http to stdio

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+import asyncio
 from source.yf_server import mcp
 import uvicorn
+
 
 def main():
     from starlette.middleware import Middleware
     from starlette.middleware.cors import CORSMiddleware
-    
+
     # Build the ASGI app
-    http_app = mcp.streamable_http_app(
-        path="/mcp",  # default mount point for HTTP transport
-    )
-    
-    uvicorn.run(http_app, host="0.0.0.0", port=8000)
+    asyncio.run(mcp.run_stdio_async())
+    # http_app = mcp.streamable_http_app(
+    #     path="/mcp",  # default mount point for HTTP transport
+    # )
+
+    # uvicorn.run(http_app, host="0.0.0.0", port=8000)
+
 
 if __name__ == "__main__":
     main()
